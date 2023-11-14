3d5edf60a983e72d6de38ad78c33f128ed6e8ea8
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-14 09:34:21 -0300

    add exercicios aula14-11

---
+++ semana02/aula-14-11/Program.cs
@@ -0,0 +1,17 @@
+﻿# region exercicio foreach
+// See https://aka.ms/new-console-template for more information
+string[] colecao = {"Item1", "Item2", "Item3", "Item4"};
+
+foreach (string item in colecao){
+    Console.WriteLine(item);
+}
+
+# endregion
+
+#region exercicio ler do teclado
+Console.Write("Digite o seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine($"Olá {nome}.");
+
+# endregion
+
