6512b27dd8061479af56115f0b02d406a78aaf59
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-16 22:50:14 -0300

    add questao 3

--- semana01/P001/Program.cs
+++ semana01/P001/Program.cs
@@ -1,6 +1,11 @@
 ﻿# region Tipos de Dados
-// 2)   Quais são os tipos de dados numéricos inteiros disponíveis no .NET? Dê exemplos de uso para cada um deles 
-//      através de exemplos.
+/*
+    2)  Quais são os tipos de dados numéricos inteiros disponíveis no .NET? 
+        Dê exemplos de uso para cada um deles através de exemplos.
+
+*/
+
+// imprimimindo tipo, limites inferior e superor e tamanho em bytes
 Console.WriteLine("Os tipos númericos inteiros disponíveis no .NET são:");
 Console.WriteLine("Tipo\t     Limite inferior\t\tLimite superior\t\tTamanho (bytes)");
 Console.WriteLine($"sbyte\t{sbyte.MinValue, 20}{sbyte.MaxValue, 27}{sizeof(sbyte), 17}");
@@ -11,15 +16,35 @@ Console.WriteLine($"int\t{int.MinValue, 20}{int.MaxValue, 27}{sizeof(int), 17}")
 Console.WriteLine($"uint\t{uint.MinValue, 20}{uint.MaxValue, 27}{sizeof(uint), 17}");
 Console.WriteLine($"long\t{long.MinValue, 20}{long.MaxValue, 27}{sizeof(long), 17}");
 Console.WriteLine($"ulong\t{ulong.MinValue, 20}{ulong.MaxValue, 27}{sizeof(ulong), 17}");
-Console.WriteLine();
+Console.WriteLine("\n");
 
 #endregion
 
 # region Conversão de Tipos
-// 3)   Suponha que você tenha uma variável do tipo double e deseja convertê-la
-//      em um tipo int. Como você faria essa conversão e o que aconteceria se a parte
-//      fracionária da variável double não pudesse ser convertida em um int? Resolva o
-//      problema através de um exemplo em C#.
+/*
+    3)  Suponha que você tenha uma variável do tipo double e deseja convertê-la em um tipo int. 
+        Como você faria essa conversão e o que aconteceria se a parte fracionária da variável 
+        double não pudesse ser convertida em um int? Resolva o problema através de um exemplo em C#.
+*/
+
+double realFracionarioDown = 42.15645325, realFracionarioUp = 42.99, realFracionarioMid = 42.5;
+double realExato = 42.0;
+int castIntDown = (int)realFracionarioDown, castIntUp = (int)realFracionarioUp, castIntMid = (int)realFracionarioMid;
+int convertIntDown = Convert.ToInt32(realFracionarioDown), convertIntUp = Convert.ToInt32(realFracionarioUp), convertIntMid = Convert.ToInt32(realFracionarioMid);
+
+Console.WriteLine("Para converter um double em int, pode ser usado um cast ou o metodo Convert.ToInt32()");
+Console.WriteLine("O cast trunca o numero, então para numeros com fração o mais indicado é o Convert");
+Console.WriteLine("Em um numero float exato o cast e o Convert retornam o mesmo valor");
+Console.WriteLine($"\tNumero double: {realFracionarioDown}");
+Console.WriteLine($"\t - Cast: {castIntDown}\n\t - Convert: {convertIntDown}");
+Console.WriteLine($"\tNumero double: {realFracionarioUp}");
+Console.WriteLine($"\t - Cast: {castIntUp}\n\t - Convert: {convertIntUp}");
+Console.WriteLine($"\tNumero double: {realFracionarioMid}");
+Console.WriteLine($"\t - Cast: {castIntMid}\n\t - Convert: {convertIntMid}");
+Console.WriteLine($"\tNumero double: {realExato}");
+Console.WriteLine($"\t - Cast: {(int)realExato}\n\t - Convert: {Convert.ToInt32(realExato)}");
+Console.WriteLine("\n");
+
 # endregion
 
 # region Operadores Aritméticos
