d86379c4ab7e0f4734eefd6daa811e437aed4556
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-20 20:28:53 -0300

    add exercecio 1 aula14-11

--- semana02/aula-14-11/Program.cs
+++ semana02/aula-14-11/Program.cs
@@ -1,111 +1,111 @@
-﻿// # region exercicio foreach
-// // See https://aka.ms/new-console-template for more information
-// string[] colecao = {"Item1", "Item2", "Item3", "Item4"};
+﻿# region exercicio foreach
+// See https://aka.ms/new-console-template for more information
+string[] colecao = {"Item1", "Item2", "Item3", "Item4"};
 
-// Console.WriteLine("USANDO FOR");
-// foreach (string item in colecao){
-//     Console.WriteLine(item);
-// }
-// Console.WriteLine();
+Console.WriteLine("USANDO FOR");
+foreach (string item in colecao){
+    Console.WriteLine(item);
+}
+Console.WriteLine();
 
-// # endregion
+# endregion
 
-// #region exercicio ler do teclado
-// Console.WriteLine("USANDO READLINE");
-// Console.Write("Digite o seu nome: ");
-// string nome = Console.ReadLine();
-// Console.WriteLine($"Olá {nome}.\n");
+#region exercicio ler do teclado
+Console.WriteLine("USANDO READLINE");
+Console.Write("Digite o seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine($"Olá {nome}.\n");
 
-// # endregion
+# endregion
 
-// #region exercicio 1
-// /*
-//     Escreva um programa em C# que imprime todos os números que são divisíveis por 3 ou por 4 entre 0 e 30;
-// */
-// Console.WriteLine("EXERCICIO 1");
-// int i;
-// for (i=0; i<=30; i++){
-//     if (i%3 == 0 || i%4 == 0){
-//         Console.WriteLine(i+ " é divisivel por 3 ou 4");
-//     }
-// }
+#region exercicio 1
+/*
+    Escreva um programa em C# que imprime todos os números que são divisíveis por 3 ou por 4 entre 0 e 30;
+*/
+Console.WriteLine("EXERCICIO 1");
+int i;
+for (i=0; i<=30; i++){
+    if (i%3 == 0 || i%4 == 0){
+        Console.WriteLine(i+ " é divisivel por 3 ou 4");
+    }
+}
 
-// #endregion
+#endregion
 
-// #region exercicio 2
-// /*
-//     Faça um programa em C# que imprima os primeiros números da série de Fibonacci até passar de 100. 
-//     A série de Fibonacci é a seguinte: 0, 1, 1, 2, 3, 5, 8, 13, 21 etc... 
-//     Para calculá-la, o primeiro elemento vale 0, o segundo vale 1, daí por diante, o n-ésimo elemento vale 
-//     o (n-1)-ésimo elemento somado ao (n-2)-ésimo elemento (ex: 8 = 5 + 3)
-// */
-// Console.WriteLine("\nEXERCICIO 2");
-// int fib0 = 0, fib1 = 1, fibn = fib0 + fib1;
-// for (i=0; fibn<=100; i++){
-//     Console.WriteLine($"fib({i}) = {fibn}");
-//     fib0 = fib1;
-//     fib1 = fibn;
-//     fibn = fib0 + fib1;
-// }
-// Console.WriteLine($"fib({i}) = {fibn}\n");
+#region exercicio 2
+/*
+    Faça um programa em C# que imprima os primeiros números da série de Fibonacci até passar de 100. 
+    A série de Fibonacci é a seguinte: 0, 1, 1, 2, 3, 5, 8, 13, 21 etc... 
+    Para calculá-la, o primeiro elemento vale 0, o segundo vale 1, daí por diante, o n-ésimo elemento vale 
+    o (n-1)-ésimo elemento somado ao (n-2)-ésimo elemento (ex: 8 = 5 + 3)
+*/
+Console.WriteLine("\nEXERCICIO 2");
+int fib0 = 0, fib1 = 1, fibn = fib0 + fib1;
+for (i=0; fibn<=100; i++){
+    Console.WriteLine($"fib({i}) = {fibn}");
+    fib0 = fib1;
+    fib1 = fibn;
+    fibn = fib0 + fib1;
+}
+Console.WriteLine($"fib({i}) = {fibn}\n");
 
-// # endregion
+# endregion
 
-// #region exercicio 3
-// /*
-// Faça um programa que imprima a seguinte tabela até o nível 8:
-// 1
-// 2   4
-// 3   6       9
-// 4   8       12      16
-// n   n*2     n*3     n*4  ...    n*n
-// */
-// Console.WriteLine("EXERCICIO 3");
-// for (i=1; i<=8; i++){
-//     for (var j=1; j<=i; j++){
-//         Console.Write($"{i*j}\t");
-//     }
-//     Console.WriteLine();
-// }
-
-
-// # endregion
-
-// # region Listas
-// // lista de inteiros
-// List<int> numeros = new List<int>();
-
-// // adicionando elementos
-// numeros.Add(1);
-// numeros.Add(2);
-// numeros.Add(3);
-// numeros.Add(4);
-// numeros.Add(5);
-
-// Console.WriteLine("LISTA DE INTEIROS");
-// foreach (int item in numeros){
-//     Console.WriteLine(item);
-// }
-
-// Console.WriteLine($"Tamanho da lista = {numeros.Count}");
-// Console.WriteLine($"Contem elemento 3? {numeros.Contains(3)}");
-// Console.WriteLine($"Contem elemento 6? {numeros.Contains(6)}");
+#region exercicio 3
+/*
+Faça um programa que imprima a seguinte tabela até o nível 8:
+1
+2   4
+3   6       9
+4   8       12      16
+n   n*2     n*3     n*4  ...    n*n
+*/
+Console.WriteLine("EXERCICIO 3");
+for (i=1; i<=8; i++){
+    for (var j=1; j<=i; j++){
+        Console.Write($"{i*j}\t");
+    }
+    Console.WriteLine();
+}
 
 
-// // lista de nomes
-// List<string> nome = new List<string>();
+# endregion
 
-// // adicionando elementos
-// nome.Add("João");
-// nome.Add("Maria");
-// nome.Add("José");
-// nome.Add("Ana");
+# region Listas
+// lista de inteiros
+List<int> numeros = new List<int>();
 
-// Console.WriteLine("\nLISTA DE NOMES");
-// foreach (string item in nome){
-//     Console.WriteLine(item);
-// }
-// # endregion
+// adicionando elementos
+numeros.Add(1);
+numeros.Add(2);
+numeros.Add(3);
+numeros.Add(4);
+numeros.Add(5);
+
+Console.WriteLine("LISTA DE INTEIROS");
+foreach (int item in numeros){
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Tamanho da lista = {numeros.Count}");
+Console.WriteLine($"Contem elemento 3? {numeros.Contains(3)}");
+Console.WriteLine($"Contem elemento 6? {numeros.Contains(6)}");
+
+
+// lista de nomes
+List<string> listaNome = new List<string>();
+
+// adicionando elementos
+listaNome.Add("João");
+listaNome.Add("Maria");
+listaNome.Add("José");
+listaNome.Add("Ana");
+
+Console.WriteLine("\nLISTA DE NOMES");
+foreach (string item in listaNome){
+    Console.WriteLine(item);
+}
+# endregion
 
 # region DateTime
 // 01/01/0001 00:00:00 ate 31/12/9999 23:59:59
@@ -119,8 +119,10 @@
 */
 string dataString = "25/10/2023", dataFormat = dataString.Replace("/", "-");
 DateTime data = DateTime.Parse(dataFormat);
-Console.WriteLine(data.ToString());
-Console.WriteLine(data.ToLongDateString());
+Console.WriteLine("Data: " + data.ToShortDateString());
+Console.WriteLine("Dia: " + data.Day);
+Console.WriteLine("Mês: " + data.Month);
+Console.WriteLine("Ano: " + data.Year);
 
 
 
