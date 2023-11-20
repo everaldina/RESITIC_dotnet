6e635587c700ef65493b344f3bb369ee0385b1df
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-20 19:53:20 -0300

    att adcionando comentarios

--- semana02/P002/Program.cs
+++ semana02/P002/Program.cs
@@ -9,6 +9,8 @@ DateTime dataVencimento;
 int opcao;
 
 while(cont){
+
+    // menu
     Console.WriteLine("\n1 - Cria tarefa");
     Console.WriteLine("2 - Lista tarefas");
     Console.WriteLine("3 - Concluir tarefa");
@@ -21,7 +23,6 @@ while(cont){
     Console.Write("Digite uma opção: ");
     opcao = Convert.ToInt32(Console.ReadLine());
 
-    // menu
     if(opcao == 0)
         cont = false;
 # region Criar tarefa
@@ -30,8 +31,12 @@ while(cont){
         titulo = Console.ReadLine();
         Console.Write("Digite a descrição da tarefa: ");
         descricao = Console.ReadLine();
-        Console.Write("Digite a data de vencimento da tarefa (dd/mm/aaaa): ");
-        dataVencimento = DateTime.Parse(Console.ReadLine());
+        // valida data
+        do{
+            Console.Write("Digite a data de vencimento da tarefa (dd/mm/aaaa): ");
+        }while(!DateTime.TryParse(Console.ReadLine(), out dataVencimento));
+
+        // cria tarefa e adiciona
         string[] tarefa = {titulo, descricao, "false"};
         listaTarefas.Add(tarefa);
         listaDataVencimento.Add(dataVencimento);
@@ -42,7 +47,7 @@ while(cont){
         Console.WriteLine($"\n    {" ", 15}Tarefa{" ", 15}Data de vencimento");
         for(var i = 0; i < listaTarefas.Count; i++){
             string[] tarefa = listaTarefas[i];
-            string concluido = tarefa[2] == "true" ? "[X]" : "[ ]";
+            string concluido = tarefa[2] == "true" ? "[X]" : "[ ]"; // se concluido = true, imprime [X], senão imprime [ ]
             Console.WriteLine($"{concluido} {tarefa[0].ToUpper()} - {tarefa[1], -30}{listaDataVencimento[i].ToShortDateString()}");
         }
     }
@@ -53,8 +58,10 @@ while(cont){
         titulo = Console.ReadLine();
 
         for(var i = 0; i<listaTarefas.Count; i++){
-            string[] tarefa = listaTarefas[i];
-            if(tarefa[0] == titulo){
+            string[] tarefa = listaTarefas[i]; // pega tarefa
+
+            // compara titulo da tarefa ao titulo digitado
+            if(tarefa[0].ToLower() == titulo.ToLower()){ 
                 listaTarefas[i][2] = "true";
                 Console.WriteLine("Tarefa concluida.");
                 break;
@@ -68,7 +75,7 @@ while(cont){
     else if(opcao ==4){
         Console.WriteLine("\nTarefas concluidas:");
         foreach(string[] tarefa in listaTarefas){
-            if(tarefa[2] == "true")
+            if(tarefa[2] == "true") // se concluido = true, imprime
                 Console.WriteLine($"{tarefa[0].ToUpper()} - {tarefa[1]}");
         }
     }
@@ -77,7 +84,7 @@ while(cont){
     else if(opcao ==5){
         Console.WriteLine("\nTarefas pendentes:");
         foreach(string[] tarefa in listaTarefas){
-            if(tarefa[2] == "false")
+            if(tarefa[2] == "false") // se concluido = false, imprime
                 Console.WriteLine($"{tarefa[0].ToUpper()} - {tarefa[1]}");
         }
     }
@@ -89,12 +96,15 @@ while(cont){
         titulo = Console.ReadLine();
         for(var i = 0; i<listaTarefas.Count; i++){
             tarefa = listaTarefas[i];
-            if(tarefa[0] == titulo){
+
+            // compara titulo da tarefa ao titulo digitado
+            if(tarefa[0].ToLower() == titulo.ToLower()){ 
                 listaTarefas.RemoveAt(i);
                 listaDataVencimento.RemoveAt(i);
+                Console.WriteLine("Tarefa removida.");
                 break;
             }
-            if(i == listaTarefas.Count - 1)
+            if(i == listaTarefas.Count - 1) // se não encontrar tarefa
                 Console.WriteLine("Tarefa não encontrada.");
         }
     }
@@ -105,7 +115,9 @@ while(cont){
         string busca = Console.ReadLine().ToLower();
         Console.WriteLine("\nTarefas encontradas:");
         for(var i = 0; i<listaTarefas.Count; i++){
-            string[] tarefa = listaTarefas[i];
+            string[] tarefa = listaTarefas[i]; // pega tarefa
+
+            // se titulo ou descricao contem palavra-chave, imprime
             if(tarefa[0].ToLower().Contains(busca) || tarefa[1].ToLower().Contains(busca)){
                 string concluido = (tarefa[2] == "true") ? "[X]" : "[ ]";
                 Console.WriteLine($"{concluido} {tarefa[0]} - {tarefa[1], -30}{listaDataVencimento[i].ToShortDateString()}");
@@ -123,13 +135,15 @@ while(cont){
             indexPrimeiraTarefa = 0;
             indexUltimaTarefa = 0;
             for(var i = 0; i<listaTarefas.Count; i++){
-                string[] tarefa = listaTarefas[i];
+                string[] tarefa = listaTarefas[i]; // pega tarefa
 
+                // incrementa contador de tarefas concluidas ou pendentes
                 if(tarefa[2] == "true")
                     concluidas++;
                 else
                     pendentes++;
 
+                // compara data de vencimento da tarefa com a data de vencimento da primeira e ultima tarefa
                 if(listaDataVencimento[i] < listaDataVencimento[indexPrimeiraTarefa])
                     indexPrimeiraTarefa = i;
                 if(listaDataVencimento[i] > listaDataVencimento[indexUltimaTarefa])
@@ -138,6 +152,7 @@ while(cont){
             Console.WriteLine($"\nTarefas concluidas: {concluidas}");
             Console.WriteLine($"Tarefas pendentes: {pendentes}");
 
+            // pega primeira e ultima tarefa e imprime
             primeiraTarefa = listaTarefas[indexPrimeiraTarefa];
             ultimaTarefa = listaTarefas[indexUltimaTarefa];
             Console.WriteLine($"Primeira tarefa: {primeiraTarefa[0]} - {primeiraTarefa[1]}");
@@ -148,7 +163,7 @@ while(cont){
         }
     }
 # endregion
-    else
+    else // se opcao invalida
         Console.WriteLine("Opção inválida.");
 }
 
