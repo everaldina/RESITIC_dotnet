cc46aeaa5db3df1bb80792a2d5e2e3fdf765346e
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-22 08:57:22 -0300

    add aula 22-11

---
+++ semana03/aula-22-11/Program.cs
@@ -0,0 +1,17 @@
+﻿#region Cultura
+using System.Globalization;
+CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("pt-BR");
+
+# endregion
+
+
+
+# region tuple
+List<(int id, string nome, DateTime dataNasc)> list = new();
+list.Add((1, "Everaldina Barbosa", new DateTime(2001, 07, 12)));
+list.Add((2, "Alberto Henrique II", new DateTime(1999, 02, 14)));
+list.Add((3, "Sabrina Guimaraes", new DateTime(2005, 11, 27)));
+
+list.ForEach(item => Console.WriteLine($"{item.id} - Nome: {item.nome} - {item.dataNasc}"));
+
+# endregion
