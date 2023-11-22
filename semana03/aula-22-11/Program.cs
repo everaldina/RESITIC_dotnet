0a557ae0200140321e34081538ca73928853f469
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-22 10:18:30 -0300

    add exemplo LINQ

--- semana03/aula-22-11/Program.cs
+++ semana03/aula-22-11/Program.cs
@@ -23,3 +23,43 @@ string[] pessoas = {"Everaldina Barbosa", "Alberto Henrique II", "Sabrina Guimar
 string[] sobrenome = {"Silva", "Ferreira"};
 Console.WriteLine($"Pessoas com sobrenome {sobrenome[0]} ou {sobrenome[1]}: {string.Join(", ", pessoas.Where(x => (x.Contains(sobrenome[0]) || x.Contains(sobrenome[1]))))}");
 #endregion
+
+
+# region LINQ
+var students = new List<Student>{
+   new Student(1, "Everaldina Barbisa", "123456789", new DateTime(2001, 07, 12), new List<string>{"123456789", "73982003551"}),
+   new Student(2, "Alberto Henrique", "987654321", new DateTime(1999, 2, 14), new List<string>{"123456789", "73987654321"}),
+   new Student(3, "Sabrina Guimaraes", "123456789", new DateTime(2005, 11, 27), new List<string>{"123456789", "77988237086"}),
+   new Student(4, "Gabriel Freitas", "843753", new DateTime(2002, 10, 20), new List<string>{"123456789"})
+};
+
+var any = students.Any();
+var anyHelder = students.Any(x => x.FullName.Contains("Helder"));
+
+var singleOrDefault = students.SingleOrDefault(x => x.Id == 10);
+
+var select = students.Select(x => x.PhoneNumbers);
+var selectMany = students.SelectMany(x => x.PhoneNumbers);
+
+var legalAge = students.Where(x => x.BirthDate <= DateTime.Today.AddYears(-18)).Select(x => x.FullName);
+Console.WriteLine($"Legal age people: {string.Join(", ", legalAge)}");
+
+# endregion
+
+
+ public class Student{
+   public Student(int id, string fullName, string document, DateTime birthDate, List<string> phoneNumbers)
+   {
+      Id = id;
+      FullName = fullName;
+      Document = document;
+      BirthDate = birthDate;
+      PhoneNumbers = new List<string>(phoneNumbers);
+   }
+
+   public int Id { get; set; }
+   public string FullName { get; set; }
+   public string Document { get; set; }
+   public DateTime BirthDate { get; set; }
+   public List<string> PhoneNumbers { get; set; }
+}
