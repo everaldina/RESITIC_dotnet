f7fdf7b11afe7a59dfc8235d89d06173714408be
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-29 10:44:59 -0300

    add commit obrigatorio

--- semana04/avaliacao/App.cs
+++ semana04/avaliacao/App.cs
@@ -1,1 +1,47 @@
 namespace Semana04.Avaliacao;
+
+class App{
+    public static void AdicionarMedico(List<Medico> medicos){
+        string nome;
+        string cpf;
+        string crm;
+        DateTime dataNascimento;
+
+        Console.WriteLine("Digite o nome do médico:");
+        nome = Console.ReadLine()!;
+        try{
+            Console.WriteLine("Digite a data de nascimento do médico:");
+            dataNascimento = DateTime.Parse(Console.ReadLine()!);
+        }catch (Exception e){
+            Console.WriteLine("Data de nascimento inválida");
+            return;
+        }
+        
+        Console.WriteLine("Digite o CPF do médico:");
+        cpf = Console.ReadLine()!;
+
+        Console.WriteLine("Digite o CRM do médico:");
+        crm = Console.ReadLine()!;
+
+        try{
+            Medico m = new Medico(nome, dataNascimento, cpf, crm);
+        }catch (Exception e){
+            Console.WriteLine(e.Message);
+            return;
+        }
+
+        if(App.ExisteMedico(medicos, cpf)){
+            Console.WriteLine("Médico já cadastrado");
+            return;
+        }
+    }
+
+    public static bool ExisteMedico(List<Medico> medicos, string cpf){
+        try{
+            Paciente p = medicos.Find(p => p.CPF == cpf);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+}
--- semana04/avaliacao/Paciente.cs
+++ semana04/avaliacao/Paciente.cs
@@ -2,5 +2,16 @@ namespace Semana04.Avaliacao;
 
 class Paciente : Pessoa{
     public string Sexo {get; set;}
-    public List<String> Sintomas => Sintomas;
+    public List<String> Sintomas = new List<String>();
+
+    public Paciente(string nome, DateTime dataNascimento, string cpf, string sexo) : base(nome, dataNascimento, cpf){
+        if (sexo.ToLower() != "masculino" && sexo.ToLower() != "feminino")
+            Sexo = sexo;
+        else
+            throw new Exception("Sexo invalido");
+    }
+
+    public void AdicionarSintoma(string sintoma){
+        Sintomas.Add(sintoma);
+    }
 }
