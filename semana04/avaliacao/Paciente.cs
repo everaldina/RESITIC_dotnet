4e411090dc16b6aacf4872b60182cfcc109ca0dd
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-29 18:06:45 -0300

    fix: verificacao de genero

--- semana04/avaliacao/Paciente.cs
+++ semana04/avaliacao/Paciente.cs
@@ -5,7 +5,7 @@ class Paciente : Pessoa{
     public List<String> Sintomas = new List<String>();
 
     public Paciente(string nome, DateTime dataNascimento, string cpf, string sexo) : base(nome, dataNascimento, cpf){
-        if (sexo.ToLower() != "masculino" || sexo.ToLower() != "feminino")
+        if (sexo.ToLower() == "masculino" || sexo.ToLower() == "feminino")
             Sexo = sexo;
         else
             throw new Exception("Sexo invalido");
