8afe7a0fe1188bfce96d92d2a82cb881698c69fd
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-29 09:51:36 -0300

    add exercicio4 aula 29-11

---
+++ semana04/exercicio4/Aluno.cs
@@ -0,0 +1,13 @@
+class Aluno{
+    public string Nome {get; set;} = "";
+    public int Idade {get; set;} = 0;
+
+    public Aluno(string nome, int idade){
+        Nome = nome;
+        Idade = idade;
+    }
+
+    public Aluno(){}
+
+
+}
---
+++ semana04/exercicio4/Program.cs
@@ -0,0 +1,8 @@
+﻿Aluno a = new Aluno();
+
+System.Console.WriteLine($"Nome: {a.Nome} | Idade: {a.Idade}");
+
+a.Nome = "João";
+a.Idade = 20;
+
+System.Console.WriteLine($"Nome: {a.Nome} | Idade: {a.Idade}");
