e2681a8c640277dd8da4d653fdd2be32cda8ce6b
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-29 11:23:14 -0300

    commit obrigatorio

--- semana04/avaliacao/App.cs
+++ semana04/avaliacao/App.cs
@@ -125,7 +125,7 @@ class App{
         }
     }
 
-    public void RelatorioPacienteEntre(List<Paciente> pacientes){
+    public static void RelatorioPacienteEntre(List<Paciente> pacientes){
         int idadeInicio, idadeFim;
 
         try{
@@ -152,18 +152,121 @@ class App{
             return;
         }
 
-        foreach (Paciente p in pacientesEntre){
+        ImprimirPacientes(pacientesEntre);
+    }
+
+    public static void ImprimirPacientes(List<Paciente> pacientes){
+        foreach (Paciente p in pacientes){
             Console.Write("Nome: " + p.Nome);
             Console.WriteLine(" | CPF: " + p.CPF);
-            Console.WriteLine("Sexo: " + p.Sexo);
-            Console.WriteLine("Data de Nascimento: " + p.DataNascimento);
-            Console.WriteLine("Idade: " + p.Idade);
-            Console.WriteLine("Sintomas: ");
+            Console.WriteLine(" - Sexo: " + p.Sexo);
+            Console.WriteLine(" - Idade: " + p.Idade);
+            Console.WriteLine(" - Data de Nascimento: " + p.DataNascimento);
+            Console.WriteLine(" - Sintomas: ");
             foreach (string s in p.Sintomas){
-                Console.WriteLine(s);
+                Console.WriteLine("\t+ " + s);
             }
             Console.WriteLine("-------------------------------");
         }
+    }
+
+    public static void RelatorioMedicoEntre(List<Medico> medicos){
+        int idadeInicio, idadeFim;
+
+        try{
+            Console.WriteLine("Digite a idade inicial:");
+            idadeInicio = int.Parse(Console.ReadLine()!);
+        }catch (Exception e){
+            Console.WriteLine("Idade inválida");
+            return;
+        }
+
+        try{
+            Console.WriteLine("Digite a idade inicial:");
+            idadeFim = int.Parse(Console.ReadLine()!);
+        }catch (Exception e){
+            Console.WriteLine("Idade inválida");
+            return;
+        }
+
+        Console.WriteLine("--------Relatório Médico Entre--------");
+
+        List<Medico> medicosEntre = medicos.FindAll(m => m.Idade >= idadeInicio && m.Idade <= idadeFim);
+        if (medicosEntre.Count == 0){
+            Console.WriteLine("Nenhum médico encontrado");
+            return;
+        }
+
+        ImprimirMedicos(medicosEntre);
+    }
+
+    public static void ImprimirMedicos(List<Medico> medicos){
+        foreach (Medico m in medicos){
+            Console.Write("Nome: " + m.Nome);
+            Console.Write(" | CPF: " + m.CPF);
+            Console.Write(" | CRM: " + m.CRM);
+            Console.WriteLine(" | Data de Nascimento: " + m.DataNascimento);
+            Console.WriteLine(" - Idade: " + m.Idade);
+            Console.WriteLine("-------------------------------");
+        }
+    }
+
+    public static void RelatorioPacienteSexo(List<Paciente> pacientes){
+        int opc;
+        string sexo;
+
+        Console.WriteLine("1 - Feminino");
+        Console.WriteLine("2 - Masculino");
+        Console.WriteLine("Digite a opção desejada:");
+
+        try{
+            opc = int.Parse(Console.ReadLine()!);
+        }catch (Exception e){
+            Console.WriteLine("Opção inválida");
+            return;
+        }
+
+        if (opc == 1)
+            sexo = "feminino";
+        else
+            sexo = "masculino";
+    
+        Console.WriteLine($"--------Relatório Paciente do Sexo {sexo}--------");
+        List<Paciente> pacientesSexo = pacientes.FindAll(p => p.Sexo == sexo);
+        if (pacientesSexo.Count == 0){
+            Console.WriteLine("Nenhum paciente encontrado");
+            return;
+        }
+
+        ImprimirPacientes(pacientesSexo);
+    }
+
+    public static void RelatorioPacientesAlfabetico(List<Paciente> pacientes){
+        Console.WriteLine("--------Relatório Paciente--------");
+
+        List<Paciente> pacientesOrdenados = pacientes.OrderBy(p => p.Nome).ToList();
+        if (pacientesOrdenados.Count == 0){
+            Console.WriteLine("Nenhum paciente encontrado");
+            return;
+        }
+
+        ImprimirPacientes(pacientesOrdenados);
+    }
+
+    public static void PacientePorSintoma(List<Paciente> pacientes){
+        string sintoma;
+
+        Console.WriteLine("--------Relatório Paciente por Sintoma--------");
+        Console.WriteLine("Digite o sintoma:");
+        sintoma = Console.ReadLine()!;
+
+        List<Paciente> pacientesSintoma = pacientes.FindAll(p => p.Sintomas.Contains(sintoma));
+        if (pacientesSintoma.Count == 0){
+            Console.WriteLine("Nenhum paciente encontrado");
+            return;
+        }
+
+        ImprimirPacientes(pacientesSintoma);
     }
 
 }
